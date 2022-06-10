af71942bb8447507cf9119f1b71e15e1db2d6b9e
juligu <67382220+juligu@users.noreply.github.com>
2022-06-10 13:36:05 -0400

    Update main.tf

--- Modules/FrontDoor/main.tf
+++ Modules/FrontDoor/main.tf
@@ -32,7 +32,7 @@ dynamic "routing_rule" {
           backend_pool_name                     = routing_rule.value.forwarding_configuration.backend_pool_name
           cache_enabled                         = routing_rule.value.forwarding_configuration.cache_enabled                           
           cache_use_dynamic_compression         = routing_rule.value.forwarding_configuration.cache_use_dynamic_compression #default: false
-          cache_query_parameter_strip_directive = routing_rule.value.forwarding_configuration.cache_query_parameter_strip_directive
+          #cache_query_parameter_strip_directive = routing_rule.value.forwarding_configuration.cache_query_parameter_strip_directive
           custom_forwarding_path                = routing_rule.value.forwarding_configuration.custom_forwarding_path
           forwarding_protocol                   = routing_rule.value.forwarding_configuration.forwarding_protocol
         }
